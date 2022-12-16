63a8938df776467bdfbcb124f58f2cf1b4f7c241
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-12-16 18:35:23 +0300

    Fixed heading render in case of link with additional text

--- src/models/__tests__/heading.test.ts
+++ src/models/__tests__/heading.test.ts
@@ -14,13 +14,30 @@ describe("Heading Model", () => {
     } as HeadingCache);
 
     it("should isLink should be true if is link", () => {
-      expect(heading.isLink).toBe(true);
+      expect(heading.hasLink).toBe(true);
     });
     it("should href should be correct", () => {
       expect(heading.href).toBe("#foo");
     });
     it("should markdownHref should be correct", () => {
       expect(heading.markdownHref).toBe("[[#foo]]");
+    });
+
+    describe("With additional text", () => {
+      const heading = new Heading({
+        heading: "[[foo]] text",
+        level: 1,
+      } as HeadingCache);
+
+      it("should isLink should be true if is link", () => {
+        expect(heading.hasLink).toBe(true);
+      });
+      it("should href should be correct", () => {
+        expect(heading.href).toBe("#foo text");
+      });
+      it("should markdownHref should be correct", () => {
+        expect(heading.markdownHref).toBe("[[#foo text]]");
+      });
     });
 
     describe("With alias", () => {
@@ -30,13 +47,30 @@ describe("Heading Model", () => {
       } as HeadingCache);
 
       it("should isLink should be true if is link", () => {
-        expect(heading.isLink).toBe(true);
+        expect(heading.hasLink).toBe(true);
       });
       it("should href should be correct", () => {
         expect(heading.href).toBe("#Something Alt Text");
       });
       it("should markdownHref should be correct", () => {
         expect(heading.markdownHref).toBe("[[#Something Alt Text|Alt Text]]");
+      });
+    });
+
+        describe("With alias and text", () => {
+      const heading = new Heading({
+        heading: "[[link|alias]] text",
+        level: 1,
+      } as HeadingCache);
+
+      it("should isLink should be true if is link", () => {
+        expect(heading.isLinkWithText).toBe(true);
+      });
+      it("should href should be correct", () => {
+        expect(heading.href).toBe("#link alias text");
+      });
+      it("should markdownHref should be correct", () => {
+        expect(heading.markdownHref).toBe("[[#link alias text|alias text]]");
       });
     });
   });
--- src/models/heading.ts
+++ src/models/heading.ts
@@ -1,4 +1,5 @@
 import { HeadingCache } from "obsidian";
+// import {read} from "obsidian-utils";
 // TODO refactor this
 export class Heading {
   constructor(private cached: HeadingCache) {}
@@ -10,33 +11,105 @@ export class Heading {
   get rawHeading(): string {
     return this.cached.heading;
   }
-  get isLink(): boolean {
+  get hasLink(): boolean {
     return /\[\[(.*?)\]\]/.test(this.cached.heading);
   }
-  get href(): string | null {
-    if (!this.isLink) return null;
-    const value = this.parseMarkdownLink(this.rawHeading);
-    const parts = value.split("|");
-    return `#${parts.join(" ")}`;
-  }
-  get markdownHref(): string | null {
-    if (!this.isLink) return `[[#${this.rawHeading}]]`;
-    const value = this.parseMarkdownLink(this.rawHeading);
-    const parts = value.split("|");
-    const hasAlias = parts.length > 1;
-    if (!hasAlias) {
-      return `[[#${parts[0]}]]`;
-    }
 
-    // The way obsidian needs to render the link is to have the link be
-    // the header + alias such as [[#Something Alt Text]]
-    // Then we need to append the actual alias
-    const link = parts.join(" ");
-    return `[[#${link}|${parts[1]}]]`;
+    get isOnlyLink(): boolean {
+    return /^\[\[(.*?)\]\]$/.test(this.cached.heading);
   }
 
-  private parseMarkdownLink(link: string): string {
-    const [, base] = link.match(/\[\[(.*?)]\]/) || [];
-    return base;
+  get isLinkWithText(): boolean {
+      return this.hasLink && !this.isOnlyLink;
+  }
+    get href(): string | null {
+        if (!this.hasLink) return null;
+
+        const attrs = this.getHeadingAttrs()
+
+        const link = attrs.link || ""
+        const alias = attrs.alias || ""
+        const addiText = attrs.additionalText || ""
+
+
+        if (attrs.alias) {
+            return `#${link} ${alias}${addiText}`
+        } else {
+            return `#${link}${addiText}`
+        }
+  }
+
+    get markdownHref(): string | null {
+        if (!this.hasLink) return `[[#${this.rawHeading}]]`;
+
+        const attrs = this.getHeadingAttrs()
+
+        const link = attrs.link || ""
+        const alias = attrs.alias || ""
+        const addiText = attrs.additionalText || ""
+
+        // return `#${link} ${alias}${addiText}`
+        // const fullHeadingAlias = `${alias}${addiText}`
+        if (attrs.alias) {
+            return `[[#${link} ${alias}${addiText}|${alias}${addiText}]]`
+        } else if (!attrs.alias) {
+            return `[[#${link}${addiText}]]`
+        }
+  }
+    private getHeadingAttrs() {
+        const input = this.rawHeading;
+
+        if (!this.hasLink) {
+            // @ts-ignore
+            return {justText: input, link: null, alias: null, additionalText: null}
+        }
+        if (this.isOnlyLink) {
+            const withoutBrackets = this.removeDoubleBrackets(input);
+            const linkParts = withoutBrackets.split("|");
+            const hasAlias = linkParts.length > 1;
+
+            if (!hasAlias) {
+                return {
+                    link: linkParts[0],
+                    alias: null,
+                    additionalText: null
+                }
+            } else {
+                return {
+                    link: linkParts[0],
+                    alias: linkParts[1],
+                    additionalText: null
+                }
+            }
+        }
+        if (this.isLinkWithText) {
+            const parts = input.split("]]");
+            const inBrackets = this.removeDoubleBrackets(parts[0]);
+            const additionalText = parts[1];
+
+            const linkParts = inBrackets.split("|");
+            const hasAlias = linkParts.length > 1;
+
+            let link: string, alias: string;
+            if (hasAlias) {
+                link = linkParts[0];
+                alias = linkParts[1];
+            } else {
+                link = linkParts[0];
+                alias = null;
+            }
+
+            return {
+                link: link,
+                alias: alias,
+                additionalText: additionalText
+            }
+        }
+  }
+
+  private removeDoubleBrackets(str: string): string {
+      str = str.replace(/\[\[/gi, '');
+      str = str.replace(/]]/gi, '');
+      return str
   }
 }
